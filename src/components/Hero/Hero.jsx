8bdaa92f32560735153fdf23617cdb8946423c9e
Tanmay Kumar Puti <130129726+Tanmayputi@users.noreply.github.com>
2025-04-03 03:32:48 +0530

    some changes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,17 +7,16 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Ada</h1>
+        <h1 className={styles.title}>Hi, I'm Tanmay</h1>
         <p className={styles.description}>
-          I'm a full-stack developer with 5 years of experience using React and
-          NodeJS. Reach out if you'd like to learn more!
+        A game developer and software enthusiast skilled in Java and React.js, passionate about building innovative and user-friendly digital solutions.
         </p>
-        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+        <a href="tanmaykumarputi@gmail.com" className={styles.contactBtn}>
           Contact Me
         </a>
       </div>
       <img
-        src={getImageUrl("hero/heroImage.png")}
+        src={getImageUrl("hero/tanmay.png")}
         alt="Hero image of me"
         className={styles.heroImg}
       />
